c71f016a4e94ee218665c9b4a2e60099f3e3eadb
reinis dreska <rdreska@gmail.com>
2021-11-26 14:22:49 +0200

    Parda tabulas un izvada datus

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 #Lai palaistu    py -m flask run
 from flask import Flask, render_template
+import mysql.connector
+import config
 
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+    host = config.host,
+    user = config.user, 
+    password = config.password,
+    database = config.db
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("USE reinis_test")
+
 @app.route("/")
-def index():
-    return render_template("index.html", title="Index")
+def tables():
+    mycursor.execute("SHOW TABLES")
+    for tables in mycursor:
+        continue
+    tables_string = "".join(tables)
+    
+    return render_template("tables.html", title="Tables", tables=tables_string)
 
 @app.route("/data")
 def data():
-    return render_template("data.html", title="Data")
+    mycursor.execute("SELECT  * FROM app_errors LIMIT 10;")
+    data_tuples = mycursor.fetchall()
+    
+    return render_template("data.html", title="Data", data=data_tuples)
 
 @app.route("/crate")
 def crate():
