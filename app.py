f12b30943378182783b0ef271221e683bdeb9abc
reinis dreska <rdreska@gmail.com>
2021-11-26 15:07:34 +0200

    izvlas tabulu

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 #Lai palaistu    py -m flask run
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 import config
+from werkzeug.datastructures import ImmutableMultiDict
 
 app = Flask(__name__)
 
@@ -22,15 +23,26 @@ def tables():
     for tables in mycursor:
         continue
     tables_string = "".join(tables)
-    
+
     return render_template("tables.html", title="Tables", tables=tables_string)
 
-@app.route("/data")
+@app.route("/which_table")
+def which_table():
+    return render_template("which_table.html", title="Which table")
+
+@app.route("/data", methods = ['POST'])
 def data():
-    mycursor.execute("SELECT  * FROM app_errors LIMIT 10;")
-    data_tuples = mycursor.fetchall()
-    
-    return render_template("data.html", title="Data", data=data_tuples)
+    if request.method == 'POST':
+        remove = ["{", "}", ":", "'"]
+        table_raw = request.form
+        table_dict = table_raw.to_dict(flat=True)
+        table_string = str(table_dict)
+        table = table_string.replace("Table", "").replace("{", "").replace("}", "").replace(":", "").replace("'", "")
+        print(table)
+        mycursor.execute("SELECT  * FROM "+ table +" LIMIT 10;")
+        data_tuples = mycursor.fetchall()
+        
+        return render_template("data.html", title="Data", data=data_tuples)
 
 @app.route("/crate")
 def crate():
