21f2fcb0194d7132ef67a12401d7b9f55ac8a09b
reinis dreska <rdreska@gmail.com>
2021-11-29 15:53:12 +0200

    datus izvada pareizi visiam tabulam

--- app.py
+++ app.py
@@ -35,25 +35,23 @@ def which_table():
 @app.route("/data", methods = ['POST'])
 def data():
     if request.method == 'POST':
-        remove = ["{", "}", ":", "'"]
         table_raw = request.form
         table_dict = table_raw.to_dict(flat=True)
         table_string = str(table_dict)
         table = table_string.replace("Table", "").replace("{", "").replace("}", "").replace(":", "").replace("'", "")
         print(table)
-        mycursor.execute("SELECT  * FROM "+ table)
+        mycursor.execute("SELECT  * FROM "+ table +" LIMIT 10")
         data_tuples = mycursor.fetchall()
-        
-        return render_template("data.html", title="Data", data=data_tuples)
+        field_names = [i[0] for i in mycursor.description]
+        return render_template("data.html", title="Data", data = data_tuples, field_names = field_names)
 
 @app.route("/crate_table")
 def crate_table():
     return render_template("crate_table.html", title="Crate Table ")
 
-@app.route("/crate", methods = ['POST'])
-def crate():
+@app.route("/crate_table_end", methods = ['POST'])
+def crate_table_end():
     if request.method == 'POST':
-        remove = ["{", "}", ":", "'"]
         table_raw = request.form
         table_dict = table_raw.to_dict(flat=True)
         table_string = str(table_dict)
@@ -62,3 +60,30 @@ def crate():
         print(table_arr)
         mycursor.execute("CREATE TABLE "+ table_arr[0] +" ( "+ table_arr[1] + " VARCHAR(100), "+ table_arr[2] + " VARCHAR(100), "+ table_arr[3] + " VARCHAR(100), "+ table_arr[4] + " VARCHAR(100), "+ table_arr[5] + " VARCHAR(100))")
         return render_template("crate.html", title="Crate ")
+
+@app.route("/crate_row")
+def crate_row():
+    return render_template("crate_row.html", title="Crate Row")
+
+@app.route("/crate_row_table", methods = ['POST'])
+def crate_row_table():
+    if request.method == 'POST':
+        table_raw = request.form
+        table_dict = table_raw.to_dict(flat=True)
+        table_string = str(table_dict)
+        table = table_string.replace("Table", "").replace("{", "").replace("}", "").replace(":", "").replace("'", "")
+        mycursor.execute("SELECT  * FROM "+ table)
+        field_names = [i[0] for i in mycursor.description]
+        return render_template("crate_row_table.html", title="Crate Row", field_names = field_names)
+
+# @app.route("/crate_row_end", methods = ['POST'])
+# def crate_row_end():
+#     if request.method == 'POST':
+#         table_raw = request.form
+#         table_dict = table_raw.to_dict(flat=True)
+#         table_string = str(table_dict)
+#         table = table_string.replace("Table", "").replace("variable_1", "").replace("variable_2", "").replace("variable_3", "").replace("variable_4", "").replace("variable_5", "").replace("{", "").replace("}", "").replace(":", "").replace("'", "").replace(",", "")
+#         table_arr = table.split()
+#         print(table_arr)
+#         mycursor.execute("CREATE TABLE "+ table_arr[0] +" ( "+ table_arr[1] + " VARCHAR(100), "+ table_arr[2] + " VARCHAR(100), "+ table_arr[3] + " VARCHAR(100), "+ table_arr[4] + " VARCHAR(100), "+ table_arr[5] + " VARCHAR(100))")
+#         return render_template("crate.html", title="Crate ")
